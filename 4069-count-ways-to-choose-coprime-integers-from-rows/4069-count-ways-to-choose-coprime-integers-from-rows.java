b54e3c15cdf73b2f59ccaf32e6013bc391bcd560
bhumi0987 <bhumibhardwaj267@gmail.com>
2025-10-28 14:17:51 +0530

    Time: 217 ms (59.9%), Space: 45.9 MB (95.27%) - LeetHub

---
+++ 4069-count-ways-to-choose-coprime-integers-from-rows/4069-count-ways-to-choose-coprime-integers-from-rows.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int mod=(int)1e9+7;
+    public int gcd(int a,int b){
+        if(b==0){
+            return a;
+        }
+        return gcd(b,a%b);
+    }
+    public int solve(int i,int curr,int n,int m,int[][] mat,int[][] dp){
+        if(i==n){
+            if(curr==1){
+                return 1;
+            }
+            return 0;
+        }
+        if(dp[i][curr]!=-1){
+            return dp[i][curr];
+        }
+        long ans=0;
+        for(int j=0;j<m;j++){
+            int next=(curr==0)?mat[i][j]:gcd(curr,mat[i][j]);
+            ans=(ans+solve(i+1,next,n,m,mat,dp))%mod;
+        }
+        return dp[i][curr]=(int)ans;
+    }
+    public int countCoprime(int[][] mat) {
+        int n=mat.length;
+        int m=mat[0].length;
+        int[][] dp=new int[n][151];
+        for(int[] it:dp){
+            Arrays.fill(it,-1);
+        }
+        return solve(0,0,n,m,mat,dp);
+    }
+}
