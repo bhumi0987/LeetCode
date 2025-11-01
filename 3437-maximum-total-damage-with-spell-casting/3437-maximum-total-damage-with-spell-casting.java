aa70918e7e62e4a3ced3dfaa33fe7009fdbf0ec2
bhumi0987 <bhumibhardwaj267@gmail.com>
2025-11-01 22:08:54 +0530

    Time: 424 ms (7.8%), Space: 167.6 MB (5%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.java
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.java
@@ -1,33 +1,27 @@
 class Solution {
-    HashMap<Integer,Integer> map;
-    long[] dp;
-    public long solve(int i,List<Integer> power){
-        if(i>=power.size()){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        long notpick=solve(i+1,power);
-        long pick=1L*power.get(i)*map.get(power.get(i));
-        if(i+1<power.size()&&power.get(i+1)!=power.get(i)+1&&power.get(i+1)!=power.get(i)+2){
-            pick+=solve(i+1,power);
-        }else if(i+2<power.size()&&power.get(i+2)!=power.get(i)+2){
-            pick+=solve(i+2,power);
-        }else if(i+3<power.size()){
-            pick+=solve(i+3,power);
-        }
-        return dp[i]=Math.max(pick,notpick);
-    }
     public long maximumTotalDamage(int[] power) {
-        map=new HashMap<>();
-        for(int it:power){
-            map.put(it,map.getOrDefault(it,0)+1);
+        TreeMap<Integer, Integer> count = new TreeMap<>();
+        for (int p : power) {
+            count.put(p, count.getOrDefault(p, 0) + 1);
         }
-        List<Integer> arr=new ArrayList<>(map.keySet());
-        Collections.sort(arr);
-        dp=new long[arr.size()];
-        Arrays.fill(dp,-1);
-        return solve(0,arr);
+        List<int[]> vec = new ArrayList<>();
+        vec.add(new int[] { -1000000000, 0 });
+        for (Map.Entry<Integer, Integer> e : count.entrySet()) {
+            vec.add(new int[] { e.getKey(), e.getValue() });
+        }
+        int n = vec.size();
+        long[] f = new long[n];
+        long mx = 0;
+        long ans = 0;
+        int j = 1;
+        for (int i = 1; i < n; i++) {
+            while (j < i && vec.get(j)[0] < vec.get(i)[0] - 2) {
+                mx = Math.max(mx, f[j]);
+                j++;
+            }
+            f[i] = mx + 1L * vec.get(i)[0] * vec.get(i)[1];
+            ans = Math.max(ans, f[i]);
+        }
+        return ans;
     }
 }
