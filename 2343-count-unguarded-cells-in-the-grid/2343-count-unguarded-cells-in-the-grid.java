485e28a1b96011933b6e9805d52241a097e6bad0
bhumi0987 <bhumibhardwaj267@gmail.com>
2025-11-02 23:15:34 +0530

    Time: 20 ms (83.94%), Space: 147.1 MB (5.11%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
@@ -1,60 +1,44 @@
 class Solution {
-    private void dfs(int r, int c, String dir, int[][] vis, Map<String, Integer> mp) {
-        int n = vis.length;
-        int m = vis[0].length;
-
-        if (r < 0 || c < 0 || r >= n || c >= m) return;
-        if (mp.containsKey(r + "," + c)) return;
-
-        vis[r][c] = 1;
-
-        switch (dir) {
-            case "r":
-                dfs(r, c + 1, "r", vis, mp);
-                break;
-            case "l":
-                dfs(r, c - 1, "l", vis, mp);
-                break;
-            case "u":
-                dfs(r - 1, c, "u", vis, mp);
-                break;
-            case "d":
-                dfs(r + 1, c, "d", vis, mp);
-                break;
-        }
-    }
-
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        int[][] vis = new int[m][n];
-        Map<String, Integer> mp = new HashMap<>();
-
+        int[][] grid = new int[m][n];
+        
+        for (int[] guard : guards) grid[guard[0]][guard[1]] = 1;
+        for (int[] wall : walls) grid[wall[0]][wall[1]] = 2;
+    
         for (int[] guard : guards) {
-            int x = guard[0], y = guard[1];
-            mp.put(x + "," + y, 1);
-            vis[x][y] = 1;
-        }
+            int row = guard[0];
+            int col = guard[1];
+            
+            for (int r = row - 1; r >= 0; r--) {
+                if (grid[r][col] == 1 || grid[r][col] == 2) break; 
+                if (grid[r][col] == 0) grid[r][col] = 3; 
+            }
 
-        for (int[] wall : walls) {
-            int x = wall[0], y = wall[1];
-            mp.put(x + "," + y, 1);
-            vis[x][y] = 1;
+            for (int r = row + 1; r < m; r++) {
+                if (grid[r][col] == 1 || grid[r][col] == 2) break;
+                if (grid[r][col] == 0) grid[r][col] = 3;
+            }
+            
+            for (int c = col - 1; c >= 0; c--) {
+                if (grid[row][c] == 1 || grid[row][c] == 2) break;
+                if (grid[row][c] == 0) grid[row][c] = 3;
+            }
+            
+            for (int c = col + 1; c < n; c++) {
+                if (grid[row][c] == 1 || grid[row][c] == 2) break;
+                if (grid[row][c] == 0) grid[row][c] = 3;
+            }
         }
-
-        for (int[] guard : guards) {
-            int r = guard[0], c = guard[1];
-            dfs(r, c + 1, "r", vis, mp);
-            dfs(r, c - 1, "l", vis, mp);
-            dfs(r + 1, c, "d", vis, mp);
-            dfs(r - 1, c, "u", vis, mp);
-        }
-
+        
         int count = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (vis[i][j] == 0) count++;
+                if (grid[i][j] == 0) {
+                    count++;
+                }
             }
         }
-
+        
         return count;
     }
 }
